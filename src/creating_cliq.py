087bc411880d178994553eb9a9469778f8b2b90f
sumedhKuku <skukunooru@gmail.com>
2016-10-21 17:53:13 -0700

    Update creating_cliq.py

--- src/creating_cliq.py
+++ src/creating_cliq.py
@@ -1,20 +1,43 @@
+
 import simplegui
 
 class Circle:
     def __init__(self):
-        self.center_point = (100,100)
-        self.radius = 20
-
+        self.radius = 40
+        self.center_point = (100,100)    
+        
+    def update_x(self, shift_x):
+        self.center_point =  (
+            self.center_point[0] + shift_x,
+            self.center_point[1]
+                )
+    def update_y(self, shift_y):
+        self.center_point =  (
+            self.center_point[0],
+            self.center_point[1] + shift_y
+                )
+        
 class ShapeAttribute:
     def __init__(self):
-        self.line_width = 5
-        self.line_color = "Blue"
-        self.fill_color = "Yellow"
+        self.line_width = 2
+        self.line_color = "Green"
+        self.fill_color = ""
 
 class Cliq:
+    
+    key_map = {
+        "up": 38,
+        "left": 37,
+        "right": 39,
+        "down": 40
+        
+        }
+    
     def __init__(self):
         self.circle_shape = Circle()
         self.shape_attributes = ShapeAttribute()
+        self.movement = 10
+        self.moveLeft = -10
         
     def draw_me(self, canvas):
         canvas.draw_circle(
@@ -23,7 +46,18 @@ class Cliq:
             self.shape_attributes.line_width,
             self.shape_attributes.line_color,
             self.shape_attributes.fill_color
-        )
+            )
+        
+    def move(self, key):
+        if key == 39:
+            self.circle_shape.update_x(self.movement)
+        if key == 37:
+            self.circle_shape.update_x(self.moveLeft)
+        if key == 38:
+            self.circle_shape.update_x(self.moveLeft)
+        
+        
+        
 
 cliq = Cliq()
 
@@ -33,6 +67,6 @@ def draw(canvas):
 # Create a frame and assign callbacks to event handlers
 frame = simplegui.create_frame("Home", 500, 500)
 frame.set_draw_handler(draw)
-
+frame.set_keydown_handler(cliq.move)
 # Start the frame animation
 frame.start()
